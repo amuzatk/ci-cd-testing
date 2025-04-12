0ce6f09cb36327d2c4e60bedbc351423bf0344a3
Kazeem Amuzat <kazeem@paydestal.com>
2025-04-12 02:23:01 +0100

    testing auto form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import './App.css'
 
 function App() {
   return (
-        <div>
-
-
-                      <p>Hello World==</p>
-      </div>
+    <div>
+      <p>Hello World==</p>
+    </div>
   )
 }
 
